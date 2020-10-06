18ac545d05d8117dd66f6b88a514a38bfef3eba7
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-06 17:13:04 -0300

    Scaffolding 2

---
+++ SoftwarePartialView/Data/SoftwarePartialViewContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SoftwarePartialView.Data
+{
+    public class SoftwarePartialViewContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public SoftwarePartialViewContext() : base("name=SoftwarePartialViewContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<SoftwarePartialView.Models.Pelicula> Peliculas { get; set; }
+    }
+}
---
+++ SoftwarePartialView/Models/Pelicula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SoftwarePartialView.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        //[Required]//Cuando Inserte en BD,va a ser requerido
+        public string Titulo { get; set; }
+        public bool EstaEnCartelera { get; set; }
+        //[StringLength(120)]//Longitud del string que vas a insertar
+        public string Genero { get; set; }
+    }
+}
