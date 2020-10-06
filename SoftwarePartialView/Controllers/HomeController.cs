03c195f8d36640b5a20823a3031c8862d555690b
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-06 12:00:55 -0300

    First PARTIAL vIEWS

---
+++ SoftwarePartialView/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using SoftwarePartialView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwarePartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var personas = new List<Persona>()
+            {
+                new Persona(){ Nombre="Marcela",Edad=18},
+                new Persona(){ Nombre="Henry",Edad=40},
+                new Persona(){ Nombre="Daniela",Edad=27}
+            };
+            ViewBag.MiListado = personas;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SoftwarePartialView/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftwarePartialView.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
